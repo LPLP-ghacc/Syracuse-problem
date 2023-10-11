c85308a329cf485b873d8ae8da46c798257eb095
LPLP <lilpophill@gmail.com>
2023-10-11 15:12:17 +0300

    Maximally optimized

--- TheCollatzProblem/Program.cs
+++ TheCollatzProblem/Program.cs
@@ -1,62 +1,44 @@
 ﻿using System;
-using ManagedCuda;
-using ManagedCuda.BasicTypes;
+using System.Collections.Generic;
 
 public class Program
 {
-    static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        await GetNumAsync();
+        GetMaxNumber();
     }
 
-    public static async Task GetNumAsync()
+    public static void GetMaxNumber()
     {
         double maxNumber = double.MinValue;
+        int maxNumberStart = 0;
+        int maxNumberSteps = 0;
 
-        for (int i = 0; i < int.MaxValue; i++)
+        for (int i = 1; i < int.MaxValue; i++)
         {
             double number = i;
-
-            var values = await GetNewValuesAsync(number, 1000);
+            List<double> values = GetNewValues(number, 1000);
 
             foreach (double value in values)
             {
                 if (value > maxNumber)
                 {
                     maxNumber = value;
-                    Console.WriteLine($"Number: {number} -- {maxNumber}");
+                    maxNumberStart = i;
+                    maxNumberSteps = values.IndexOf(value);
                 }
             }
+
+            if (i % 10000 == 0)
+            {
+                Console.WriteLine($"Processed {i} numbers. Max number: {maxNumber} (Start: {maxNumberStart}, Steps: {maxNumberSteps})");
+            }
         }
+
+        Console.WriteLine($"Final Max Number: {maxNumber} (Start: {maxNumberStart}, Steps: {maxNumberSteps})");
     }
 
     private static List<double> GetNewValues(double number, int maxSteps)
-    {
-        var values = new List<double>();
-
-        using (CudaContext context = new CudaContext())
-        {
-            double[] hostValues = new double[maxSteps];
-            CudaDeviceVariable<double> deviceValues = new CudaDeviceVariable<double>(maxSteps);
-
-            for (int i = 0; i < maxSteps; i++)
-            {
-                // 3x + 1 problem
-                number = number % 2 != 0 ? number * 3 + 1 : number / 2;
-                hostValues[i] = number;
-            }
-
-            deviceValues.CopyToDevice(hostValues);
-
-            deviceValues.CopyToHost(hostValues);
-
-            values.AddRange(hostValues);
-        }
-
-        return values;
-    }
-
-    private static async Task<List<double>> GetNewValuesAsync(double number, int maxSteps)
     {
         var values = new List<double>();
 
